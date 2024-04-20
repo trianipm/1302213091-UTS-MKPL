4a440ae109244e02b553cc3e1941cec5351290dc
trianipm <127008970+trianipm@users.noreply.github.com>
2024-04-21 04:03:29 +0700

    Membuat method kecil-TaxFunction.java
    Membuat method dengan memecahnya menjadi beberapa method yang lebih kecil dan terpisah sesuai fungsinya, seperti checkWorkingMonths, limitNumberOfChildren, calculateTaxExemption, dan calculateTaxableIncome.

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -2,43 +2,23 @@ package lib;
 
 public class TaxFunction {
 
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
+			boolean isMarried, int numberOfChildren) {
+		// Mengecek apakah jumlah bulan kerja melebihi 12 bulan
+		checkWorkingMonths(numberOfMonthWorking);
+
+		// Membatasi jumlah anak maksimal yang diproses
+		numberOfChildren = limitNumberOfChildren(numberOfChildren);
+
+		// Menghitung penghasilan tidak kena pajak
+		int taxExemption = calculateTaxExemption(isMarried, numberOfChildren);
+
+		// Menghitung pajak
+		int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible,
+				isMarried, taxExemption, numberOfChildren);
+
+		// Mengembalikan nilai pajak
+		return Math.max(0, (int) Math.round(0.05 * taxableIncome));
 	}
-	
+
 }
