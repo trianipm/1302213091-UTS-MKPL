c349fe630ed6325d798e96363ecedfefca9d2c2c
trianipm <127008970+trianipm@users.noreply.github.com>
2024-04-21 04:21:26 +0700

    Bad Smells Long Method-TaxFunction.java
    Long Method: Method calculateTax pada file yang belum direfactorisasi memiliki banyak baris kode yang kompleks dan sulit dipahami. Setelah refactorisasi, kita memecahnya menjadi beberapa method yang lebih kecil dan terpisah sesuai fungsinya, seperti checkWorkingMonths, limitNumberOfChildren, calculateTaxExemption, dan calculateTaxableIncome.

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -2,25 +2,26 @@ package lib;
 
 public class TaxFunction {
 
+	// BAD SMELLS LONG METHOD
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
-			boolean isMarried, int numberOfChildren) { // bad smells conditional complexity
-		// // Mengecek apakah jumlah bulan kerja melebihi 12 bulan
-		// checkWorkingMonths(numberOfMonthWorking);
+			boolean isMarried, int numberOfChildren) {
+		// Mengecek apakah jumlah bulan kerja melebihi 12 bulan
+		checkWorkingMonths(numberOfMonthWorking);
 
-		// // Membatasi jumlah anak maksimal yang diproses
-		// numberOfChildren = limitNumberOfChildren(numberOfChildren);
+		// Membatasi jumlah anak maksimal yang diproses
+		numberOfChildren = limitNumberOfChildren(numberOfChildren);
 
-		// // Menghitung penghasilan tidak kena pajak
-		// int taxExemption = calculateTaxExemption(isMarried, numberOfChildren);
+		// Menghitung penghasilan tidak kena pajak
+		int taxExemption = calculateTaxExemption(isMarried, numberOfChildren);
 
-		// // Menghitung pajak
-		// int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome,
-		// numberOfMonthWorking, deductible,
-		// isMarried, taxExemption, numberOfChildren);
+		// Menghitung pajak
+		int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome,
+		numberOfMonthWorking, deductible,
+		isMarried, taxExemption, numberOfChildren);
 
-		// // Mengembalikan nilai pajak
-		// return Math.max(0, (int) Math.round(0.05 * taxableIncome));
-		// }
+		// Mengembalikan nilai pajak
+		return Math.max(0, (int) Math.round(0.05 * taxableIncome));
+		}
 
 		// // Memeriksa apakah jumlah bulan kerja melebihi 12 bulan
 		// private static void checkWorkingMonths(int numberOfMonthWorking) {
